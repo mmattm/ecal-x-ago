722b289c82d7ee227a4e80902e69a3b269f6a2c3
Matthieu Minguet <minguet.mat@gmail.com>
2024-11-06 23:13:11 +0100

    log

--- src/CameraPathAnimator.jsx
+++ src/CameraPathAnimator.jsx
@@ -268,11 +268,11 @@ export default function CameraPathAnimator({
       const camera = cameraControlsRef.current.camera; // Access the camera directly
 
       // Log the camera's position in the desired format [-x, y, z]
-      console.log(
-        `[${camera.position.x.toFixed(2)}, ${camera.position.y.toFixed(
-          2
-        )}, ${camera.position.z.toFixed(2)}],`
-      );
+      // console.log(
+      //   `[${camera.position.x.toFixed(2)}, ${camera.position.y.toFixed(
+      //     2
+      //   )}, ${camera.position.z.toFixed(2)}],`
+      // );
 
       // Update camera controls
       cameraControlsRef.current.update(delta);
